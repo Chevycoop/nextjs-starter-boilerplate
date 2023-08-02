0c2a9cdf2afc46d15c529de57bf2b3318f5a8016
Chevycoop <129355405+Chevycoop@users.noreply.github.com>
2023-08-01 21:38:38 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import Layout from '../components/Layout'
-import BlogList from '../components/BlogList'
+// pages/index.js
+
+import matter from "gray-matter"
+import Layout from "../components/Layout"
+import BlogList from "../components/BlogList"
 
 const Index = props => {
   return (
@@ -9,7 +12,7 @@ const Index = props => {
       siteDescription={props.description}
     >
       <section>
-        <BlogList/>
+        <BlogList allBlogs={props.allBlogs} />
       </section>
     </Layout>
   )
@@ -18,10 +21,44 @@ const Index = props => {
 export default Index
 
 export async function getStaticProps() {
+  // getting the website config
   const siteConfig = await import(`../data/config.json`)
+
+  const webpackContext = require.context("../posts", true, /\.md$/)
+  // the list of file names contained
+  // inside the "posts" directory
+  const keys = webpackContext.keys()
+  const values = keys.map(webpackContext)
+
+  // getting the post data from the files contained
+  // in the "posts" folder
+  const posts = keys.map((key, index) => {
+    // dynamically creating the post slug
+    // from file name
+    const slug = key
+      .replace(/^.*[\\\/]/, "")
+      .split(".")
+      .slice(0, -1)
+      .join(".")
+
+    // getting the .md file value associated
+    // with the current file name
+    const value = values[index]
+
+    // parsing the YAML metadata and markdown body
+    // contained in the .md file
+    const document = matter(value.default)
+
+    return {
+      frontmatter: document.data,
+      markdownBody: document.content,
+      slug,
+    }
+  })
 
   return {
     props: {
+      allBlogs: posts,
       title: siteConfig.default.title,
       description: siteConfig.default.description,
     },
